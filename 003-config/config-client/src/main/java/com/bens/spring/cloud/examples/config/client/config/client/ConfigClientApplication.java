7edc660c8aa8e5a67cf0b501f5ddcc1d51ca32d3
gleamy_ming <gleamy_ming@126.com>
2019-02-16 10:59:22 +0800

    config server test

---
+++ 003-config/config-client/src/main/java/com/bens/spring/cloud/examples/config/client/config/client/ConfigClientApplication.java
@@ -0,0 +1,27 @@
+package com.bens.spring.cloud.examples.config.client.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${bens.env.name}")
+    private String envName;
+
+    @GetMapping("hello-config")
+    public Object helloConfig() {
+        return envName;
+    }
+}
+
