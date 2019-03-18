f07f1a6f6d3d8bea05d3c3c1b513dc0887fa4a3f
gleamy_ming <gleamy_ming@126.com>
2019-03-18 13:30:56 +0800

    Bens casual test

---
+++ 999-bensCasualTest/src/main/java/com/bens/spring/casual/test/BensCasualTestApplication.java
@@ -0,0 +1,13 @@
+package com.bens.spring.casual.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BensCasualTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BensCasualTestApplication.class, args);
+    }
+
+}
